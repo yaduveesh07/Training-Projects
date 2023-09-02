2bc6f67e10fb8adabbcb21fde7d47aeef4600a49
Yaduveesh Pratap <yaduveeshp.singh@grovvey.com>
2023-09-02 09:22:50 -0700

    fix error

--- cwh-todo-list/src/app/MyComponent/todos/Todo.ts
+++ cwh-todo-list/src/app/MyComponent/todos/Todo.ts
@@ -1,7 +1,6 @@
 export class Todo{
-    sno: number
-    title: string
-    desc: string
-    active: boolean
-    
+    sno: number=0    
+    title: string=''
+    desc: string=''
+    active: boolean=false
 }
