c7e67c8bcdca36bb6fcd474d47610404d455cb5d
Yaduveesh Pratap <yaduveeshp.singh@grovvey.com>
2023-09-03 22:58:21 -0700

    converted inline to html files

--- Hello-world/src/app/app.component.ts
+++ Hello-world/src/app/app.component.ts
@@ -9,16 +9,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     HomeComponent,
   ],
-  template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
-`,
+  templateUrl:'./app.component.html',
   styleUrls: ['./app.component.css']
   
 })
--- Hello-world/src/app/housing-location/housing-location.component.ts
+++ Hello-world/src/app/housing-location/housing-location.component.ts
@@ -7,13 +7,7 @@ import { HousingLocation } from '../housinglocation';
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
-  template: `
-  <section class="listing">
-    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
-  </section>
-  `,
+  templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
